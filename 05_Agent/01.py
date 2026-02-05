3bbbf8e6caf2dc2c949db7f4380cade0dda2f093
Abrar ul riyaz <abrar_2022bite057@nitsri.ac.in>
2026-02-05 21:58:42 +0530

    update

--- 05_Agent/01.py
+++ 05_Agent/01.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 from langchain.tools import tool
 from langchain.messages import HumanMessage, ToolMessage
+from langchain.agents import create_agent
 import os
 
 load_dotenv()
@@ -39,31 +40,38 @@ llm=HuggingFaceEndpoint(
 )
 model=ChatHuggingFace(llm=llm)
 
-llm_with_tools=model.bind_tools([get_current_weather])
+agent=create_agent(
+    model=model,
+    tools=[get_current_weather],
+    system_prompt="You are helpfull Assistant"
+)
 
-user_input=input("enter locaton")
-user_input=f"What is the current weather at {user_input} Give response in paragraph"
+result=agent.invoke(
+    {
+        "messages":[{"role":"user","content":"What clothes should i wear if i go out right now in auckland newzealand, Give answer in paragraph"}]
+    }
+)
 
-messages=[HumanMessage(content=user_input)]
 
-result1=llm_with_tools.invoke(messages)
-print(result1.tool_calls)
+print(result['messages'][-1].content)
 
-messages.append(result1)
 
-for tool_call in result1.tool_calls:
-    if tool_call['name']=='get_current_weather':
-        response=get_current_weather.invoke(tool_call['args'])
-
-        messages.append(
-            ToolMessage(
-                content=str(response),
-                tool_call_id=tool_call['id']
-            )
-        )
-
-result2=llm_with_tools.invoke(messages)
-
-messages.append(result2)
-
-print(result2.content)
+# llm_with_tools=model.bind_tools([get_current_weather])
+# user_input=input("enter locaton")
+# user_input=f"What is the current weather at {user_input} Give response in paragraph"
+# messages=[HumanMessage(content=user_input)]
+# result1=llm_with_tools.invoke(messages)
+# print(result1.tool_calls)
+# messages.append(result1)
+# for tool_call in result1.tool_calls:
+#     if tool_call['name']=='get_current_weather':
+#         response=get_current_weather.invoke(tool_call['args'])
+#         messages.append(
+#             ToolMessage(
+#                 content=str(response),
+#                 tool_call_id=tool_call['id']
+#             )
+#         )
+# result2=llm_with_tools.invoke(messages)
+# messages.append(result2)
+# print(result2.content)
